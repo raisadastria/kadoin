eb627ffed27984bf8aa2cf576de92bfb5e84b8b1
Devi Risvianti <devirisvianti28@gmail.com>
2026-05-24 17:11:15 +0700

    feat: tambah modal konfirmasi logout pembeli

--- includes/header.php
+++ includes/header.php
@@ -75,7 +75,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
                         <a href="#" title="Profil Saya" class="nav-icon"> 
                             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/></svg>
                         </a>
-                        <a href="<?php echo BASE_URL; ?>logout.php">Logout</a>
+                        <a href="#" onclick="bukaModalLogout(); return false;">Logout</a>
                         <?php if (isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'admin'): ?>
                             <a href="<?php echo BASE_URL; ?>admin/">Admin</a>
                         <?php endif; ?>
